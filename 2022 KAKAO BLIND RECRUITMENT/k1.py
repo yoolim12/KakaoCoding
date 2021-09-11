8ce42a95b46f56eaa52ca3526c73cdcdbc7daab5
yoolim12 <78739536+yoolim12@users.noreply.github.com>
2021-09-12 02:59:37 +0900

    Update k1.py

--- 2022 KAKAO BLIND RECRUITMENT/k1.py
+++ 2022 KAKAO BLIND RECRUITMENT/k1.py
@@ -1,13 +1,12 @@
 def solution(id_list, report, k):
-    who = {}
-    send = {}
+    who = {} # 누구를 누가 신고했는지 ex) "muzi" : ["apeach", "ryan"] --> apeach랑 ryan이 muzi를 신고
+    send = {} # 누구에게 메일을 몇 통 보낼지 ex) "muzi": 2 --> muzi에게 2통
     for e in id_list:
         send[e] = 0
-        who[e] = set()
+        who[e] = set() # muzi에게 apeach가 여러번 신고 넣어도 한번으로 취급. 즉 중복을 피하기 위해 set 사용
     for e in report:
         l = e.split()
         who[l[1]].add(l[0])
-        #count[l[1]] += 1
     for elem in who:
         if len(who[elem]) >= k:
             for e in who[elem]:
